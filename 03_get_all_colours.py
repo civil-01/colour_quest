4791d57c1d0e1c8c7afa31fc3b76a08d414104b1
civil-01 <101526906+civil-01@users.noreply.github.com>
2025-03-12 11:04:35 +1300

    started integrating to game component v2

--- 03_get_all_colours.py
+++ 03_get_all_colours.py
@@ -1,6 +1,16 @@
 import csv
 import random
 
+def round_ans(val):
+    """
+    Rounds numbers to nearest integer
+    :param val: number to be rounded.
+    :return: Rounded number (an integer)
+    """
+    var_rounded = (val * 2 + 2) // 2
+    raw_unrounded = "{:.0f}".format(var_rounded)
+    return int(raw_unrounded)
+
 # retrieve colours from csv anf put them in a list
 file = open("colour_quest/00_colour_list_hex_v3.csv", "r")
 all_colours = list(csv.reader(file, delimiter=","))
@@ -9,4 +19,30 @@ file.close()
 
 # remove the first row
 all_colours.pop(0)
-print(all_colours)
+
+round_colours = []
+colour_scores = []
+
+# loop until we have four colours with different scores
+while len(round_colours) < 4:
+    potential_colour = random.choice(all_colours)
+
+    # get the score and check its not a duplicate
+    if potential_colour[1] not in colour_scores:
+        round_colours.append(potential_colour)
+        colour_scores.append(potential_colour[1])
+
+print(round_colours)
+print(colour_scores)
+
+#  find target scores
+
+# change score to integers
+int_scores = [int(x) for x in colour_scores]
+print("Scores Unsorted", int_scores)
+int_scores.sort()
+print("Sorted Scores", int_scores)
+
+median = (int_scores[1] + int_scores[2]) / 2
+print(f"unrounded median: {median}")
+print(f"median: {median:.0f}")
